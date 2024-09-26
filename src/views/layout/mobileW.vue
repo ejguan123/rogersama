3da682a7eabaf43ab16d47236aa02a00aa950f0c
ejguan123 <ej_guan@tk3c.com>
2024-09-26 16:37:14 +0800

    修改項目名稱

--- src/views/layout/mobileW.vue
+++ src/views/layout/mobileW.vue
@@ -1,5 +1,4 @@
 <template>
- 
   <!-- 手機版上方選單 -->
   <div class="nav-footer">
     <div class="footer-bg"></div>
@@ -12,9 +11,17 @@
 
     <div class="box-area social">
       <ul>
-        <li><a href="https://reurl.cc/QbZ149" target="_blank"><i class="fa-brands fa-meta"></i></a></li>
-        <li><a href="https://reurl.cc/7pMZVl" target="_blank"><i class="fa-brands fa-line"></i></a></li>
-        <li><a href="https://reurl.cc/3Ye8kX" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+        <li>
+          <a href="https://reurl.cc/QbZ149" target="_blank"><i class="fa-brands fa-meta"></i></a>
+        </li>
+        <li>
+          <a href="https://reurl.cc/7pMZVl" target="_blank"><i class="fa-brands fa-line"></i></a>
+        </li>
+        <li>
+          <a href="https://reurl.cc/3Ye8kX" target="_blank"
+            ><i class="fa-brands fa-instagram"></i
+          ></a>
+        </li>
       </ul>
     </div>
   </div>
@@ -26,18 +33,18 @@
     <div class="top-nav">
       <h3 class="title">快速選單</h3>
       <ul class="a1">
-          <li><a href="#tab1">電競筆電</a></li>
-          <li><a href="#tab2">電競桌機</a></li>
-          <li><a href="#tab3">電競螢幕</a></li>
-          <li><a href="#tab4">鍵鼠/耳機</a></li>
-          <li><a href="#tab5">顯卡</a></li>
-          <li><a href="#tab6">SSD/RAM</a></li>
-        </ul>
-        <ul class="a2">
-          <li><a href="#tab7">Wi-PS5 & 週邊</a></li>
-          <li><a href="#tab8">電視</a></li>
-          <li><a href="#tab9">聲霸/喇叭</a></li>
-        </ul>
+        <li><a href="#tab1">電競筆電</a></li>
+        <li><a href="#tab2">電競桌機</a></li>
+        <li><a href="#tab3">電競螢幕</a></li>
+        <li><a href="#tab4">鍵鼠/耳機</a></li>
+        <li><a href="#tab5">顯卡</a></li>
+        <li><a href="#tab6">SSD/RAM</a></li>
+      </ul>
+      <ul class="a2">
+        <li><a href="#tab7">PS5 & 週邊</a></li>
+        <li><a href="#tab8">電視</a></li>
+        <li><a href="#tab9">聲霸/喇叭</a></li>
+      </ul>
     </div>
     <a class="switch"><i class="fa-solid fa-angle-down"></i></a>
   </div>
