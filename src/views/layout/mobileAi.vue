ad8027cca64bbcdc9feb6b6ccd12a7126c64c739
ejguan123 <ej_guan@tk3c.com>
2024-09-30 18:13:49 +0800

    新增 ai頁面手機板選單

---
+++ src/views/layout/mobileAi.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- 手機版上方選單 -->
+  <div class="nav-footer">
+    <div class="footer-bg"></div>
+    <div class="nav-box">
+      <ul>
+        <li data-id="top">回到上層</li>
+        <li data-id="1">關注社群</li>
+      </ul>
+    </div>
+
+    <div class="box-area social">
+      <ul>
+        <li>
+          <a href="https://reurl.cc/QbZ149" target="_blank"><i class="fa-brands fa-meta"></i></a>
+        </li>
+        <li>
+          <a href="https://reurl.cc/7pMZVl" target="_blank"><i class="fa-brands fa-line"></i></a>
+        </li>
+        <li>
+          <a href="https://reurl.cc/3Ye8kX" target="_blank"
+            ><i class="fa-brands fa-instagram"></i
+          ></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- go top -->
+  <a class="gotop-mobile"></a>
+
+  <div class="mobile-for-product">
+    <div class="top-nav">
+      <h3 class="title">快速選單</h3>
+      <ul class="a1">
+        <li><a href="#tab1">輕薄商務</a></li>
+        <li><a href="#tab2">飆速電競</a></li>
+        <li><a href="#tab3">特仕改裝升級</a></li>
+        <li><a href="#tab4">精選螢幕</a></li>
+        <li><a href="#tab5">高效桌機</a></li>
+        <li><a href="#tab6">DIY零組件</a></li>
+        <li><a href="#event">熱門活動 </a></li>
+      </ul>
+      <ul class="a2">
+        <li><a href="#tab7">Wi-Fi路由器</a></li>
+        <li><a href="#tab8">速速GO</a></li>
+        <li><a href="#tab9">鍵盤滑鼠</a></li>
+        <li><a href="#tab10">硬碟</a></li>
+        <li><a href="#tab11">隨身碟/記憶卡</a></li>
+        <li><a href="#tab12">投影機/監控設備</a></li>
+        <li><a href="#tab13">辦公設備</a></li>
+        <li><a href="#tab14">出清搶便宜 </a></li>
+        <li><a href="#event">熱門活動 </a></li>
+      </ul>
+    </div>
+    <a class="switch"><i class="fa-solid fa-angle-down"></i></a>
+  </div>
+</template>
