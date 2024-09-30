8171ea321f51c93fbd986e798841ff66a3aee1d9
ejguan123 <ej_guan@tk3c.com>
2024-09-30 18:14:47 +0800

    新增 banner輪播翻轉共用

---
+++ src/views/layout/banner.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { toRefs } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { EffectFlip } from 'swiper/modules'
+
+const props = defineProps({
+  banner1: Object,
+  banner2: Object
+})
+
+const { banner1 } = toRefs(props)
+const { banner2 } = toRefs(props)
+</script>
+
+<template>
+  <!-- 熱門活動 -->
+  <section class="event-box scroll" id="event">
+    <div class="content w:full">
+      <!-- 左邊banner輪播翻轉 -->
+      <div class="banner1">
+        <swiper
+          class="overflow:hidden"
+          :loop="true"
+          :autoplay="{
+            delay: 1600,
+            disableOnInteraction: false
+          }"
+          :effect="'flip'"
+          :flipEffect="{
+            slideShadows: false
+          }"
+          :modules="[EffectFlip]"
+        >
+          <swiper-slide v-for="b1 in banner1">
+            <a :href="$filters.addGALink(b1.url)" target="_blank">
+              <img :src="$filters.siteUrl(b1.image)" />
+            </a>
+          </swiper-slide>
+        </swiper>
+      </div>
+
+      <!-- 右邊banner輪播翻轉 -->
+      <div class="banner2">
+        <swiper
+          class="overflow:hidden"
+          :loop="false"
+          :autoplay="{
+            delay: 1600,
+            disableOnInteraction: false
+          }"
+          :effect="'flip'"
+          :flipEffect="{
+            slideShadows: false
+          }"
+          :modules="[EffectFlip]"
+        >
+          <swiper-slide v-for="b2 in banner2">
+            <a :href="$filters.addGALink(b2.url)" target="_blank">
+              <img :src="$filters.siteUrl(b2.image)" />
+            </a>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+  </section>
+</template>
