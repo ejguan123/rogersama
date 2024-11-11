6c6eb5e812df9cfa35934461441ede57711d92ad
ejguan123 <ej_guan@tk3c.com>
2024-11-11 10:15:15 +0800

    載入中套件

---
+++ src/components/Loading.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { toRefs } from 'vue'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
+
+const props = defineProps({
+  status: Boolean
+})
+
+const { status } = toRefs(props)
+</script>
+
+<template>
+  <div class="rel">
+    <Loading :can-cancel="true" :active.snyc="status" :color="'#FF0000'" :is-full-page="false">
+    </Loading>
+  </div>
+</template>
