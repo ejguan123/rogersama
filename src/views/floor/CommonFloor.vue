3933587b57781b6bdf90e6c50a885906821cedd4
ejguan123 <ej_guan@tk3c.com>
2024-11-19 16:58:33 +0800

    新增共用商品樓層

---
+++ src/views/floor/CommonFloor.vue
@@ -0,0 +1,30 @@
+<script setup>
+import listF from '../layout/listF.vue'
+</script>
+
+<script>
+import { globalMixin } from '../../globalMixin.js'
+
+export default {
+  mixins: [globalMixin],
+  props: ['floors', 'menu'],
+  mounted() {
+    this.getFloorData(this.menu)
+  }
+}
+</script>
+
+<template>
+  <section class="floor" v-for="(floor, f) in floors" :key="f">
+    <h2 class="title">
+      <a :name="`pro${menu[f]}`" :id="`pro${menu[f]}`"> </a>
+      <img :src="$filters.siteUrl(floor.image)" />
+    </h2>
+
+    <component
+      v-if="products[menu[f]] != undefined"
+      :is="listF"
+      :pro="products[menu[f]].Data"
+    ></component>
+  </section>
+</template>
