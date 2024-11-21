2bc2b3959d602ed39c79d734ed59dccc60f3655a
ejguan123 <ej_guan@tk3c.com>
2024-11-21 18:00:05 +0800

    新增其他版型選擇

--- src/views/floor/CommonFloor.vue
+++ src/views/floor/CommonFloor.vue
@@ -1,10 +1,11 @@
 <script setup>
 import listF from '../layout/listF.vue'
+import listM from '../layout/listM.vue'
+import listD from '../layout/listD.vue'
 </script>
 
 <script>
 import { globalMixin } from '../../globalMixin.js'
-
 export default {
   mixins: [globalMixin],
   props: ['floors', 'menu'],
@@ -15,15 +16,17 @@ export default {
 </script>
 
 <template>
-  <section class="floor" v-for="(floor, f) in floors" :key="f">
+  <section class="floor scroll" v-for="(floor, f) in floors" :key="f">
     <h2 class="title">
-      <a :name="`pro${menu[f]}`" :id="`pro${menu[f]}`"> </a>
-      <img :src="$filters.siteUrl(floor.image)" />
+      <a v-if="floor.url" :href="$filters.addGALink(floor.url)" :name="`pro${menu[f]}`" :id="`pro${menu[f]}`"> 
+         <img :src="$filters.siteUrl(floor.image)" />
+      </a>
+      <img v-else :src="$filters.siteUrl(floor.image)" />
     </h2>
 
     <component
       v-if="products[menu[f]] != undefined"
-      :is="listF"
+      :is="floor.type == 'listM' ? listM : listF"
       :pro="products[menu[f]].Data"
     ></component>
   </section>
